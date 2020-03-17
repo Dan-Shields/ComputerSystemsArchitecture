55f65012f17ec54d3fae36b5b89de6b0ced15503
Dan Shields <d.shieldsuk@gmail.com>
2020-03-17 01:47:10 +0000

    final submission

--- csa.c
+++ csa.c
@@ -6,18 +6,17 @@
 #include <string.h>
 #include <math.h>
 
-// Number of total cache blocks
+// Total number of cache blocks
 #define cacheSize 256
 
-#define addressLength 20
-
-int run (FILE traceFile[50], char traceFileName[50], int mode) {
+// Function which takes file stream, name of the file and what cache mode to run and prints out the results for that config
+int runCacheTest (FILE traceFile[50], char traceFileName[50], int cacheModeId) {
 
     // Number of words per cache block
-    int cacheBlockSize = (int) pow(2, mode-1);
+    int cacheBlockSize = (int) pow(2, cacheModeId-1);
 
-    // Number of cache lines
-    int cacheLength = cacheSize / cacheBlockSize;
+    // Number of cache blocks
+    int cacheBlockCount = cacheSize / cacheBlockSize;
 
     // Declare cache array and initialise to invalid values (-1) to prevent address 0x000 returning a hit when array is supposed to be empty
     int cache[cacheSize];
@@ -28,22 +27,33 @@ int run (FILE traceFile[50], char traceFileName[50], int mode) {
     // Declare array for storing dirty bits (intially all unset)
     int dirtyFlags[cacheSize] = {0};
 
-    // Stores block ID that should to be written to next
-    int nextBlock = 0;
+    // Stores block index that should to be written to next
+    int nextBlockIndex = 0;
 
+    // Declare counters and init to 0
     int NRA = 0, NWA = 0, NCRH = 0, NCRM = 0, NCWH = 0, NCWM = 0;
 
-    int line = 0;
     while (1) {
         char mode;
         int address;
 
-        if (fscanf(traceFile, "%c %x\n", &mode, &address) != 2) break; // stop when end of file reached
+        // Read next line of file, stop when EoF reached
+        if (fscanf(traceFile, "%c %x\n", &mode, &address) != 2) break; 
+        
+        // Get tag bits by shifting address right a certain number of places
+        int tag = address >> (cacheModeId - 1);
 
         int hit = 0;
-        int hitLocation; 
-        for (int i = 0; i < cacheLength; i++) {
-            if (address == cache[i]) {
+        int hitLocation;
+
+        // Loop through cache array searching for a match
+        for (int i = 0; i < cacheBlockCount; i++) {
+            int foundAddress = cache[i];
+
+            // Get tag bits by shifting address right a certain number of places
+            int foundTag = foundAddress >> (cacheModeId - 1);
+
+            if (tag == foundTag) {
                 // Cache hit
                 hit = 1;
                 hitLocation = i; // Used in write hit operations to mark the block as dirty
@@ -51,58 +61,57 @@ int run (FILE traceFile[50], char traceFileName[50], int mode) {
             }
         }
 
+        // Once search is complete, act based upon what happened (4 possibilities)
         if (mode == 'R') {
             if (hit) {
-                // Cache read hit
+                // READ HIT //
                 NCRH++;
             } else {
-                // Cache read miss
+                // READ MISS //
                 NCRM++;
 
-                int cacheBlockID = nextBlock++ % cacheLength; // Locate cache block for overwriting
+                int cacheBlockID = nextBlockIndex++ % cacheBlockCount; // Locate cache block for overwriting
                 if (dirtyFlags[cacheBlockID]) { // If block is dirty
-                    NWA++; // Write its previous data back to the lower memory
+                    NWA += cacheBlockSize; // Write its previous data back to the lower memory
                 }
 
-                NRA++; // Read data from lower memory into the cache block
+                NRA += cacheBlockSize; // Read data from lower memory into the cache block
                 cache[cacheBlockID] = address;
 
                 dirtyFlags[cacheBlockID] = 0; // Mark cache block not-dirty
             }
-        } else {
+        } else if (mode == 'W') {
             if (hit) {
-                // Cache write hit
+                // WRITE HIT //
                 NCWH++;
 
                 dirtyFlags[hitLocation] = 1; // Mark cache block dirty
             } else {
-                // Cache write miss
+                // WRITE MISS //
                 NCWM++;
 
-                int cacheBlockID = nextBlock++ % cacheLength; // Locate cache block for overwriting
+                int cacheBlockID = nextBlockIndex++ % cacheBlockCount; // Locate cache block for overwriting
                 if (dirtyFlags[cacheBlockID]) { // If block is dirty
-                    NWA++; // Write its previous data back to the lower memory
+                    NWA += cacheBlockSize; // Write its previous data back to the lower memory
                 }
 
-                NRA++; // Read data from lower memory into the cache block
+                NRA += cacheBlockSize; // Read data from lower memory into the cache block
 
                 cache[cacheBlockID] = address; // Write the new data into the cache block
 
                 dirtyFlags[cacheBlockID] = 1; // Mark cache block dirty
             }
         }
-
-        line++;
     }
-    fclose(traceFile);
 
-    printf("%s,%d,NRA: %d, NWA: %d, NCRH: %d, RCRM: %d, NCWH: %d, NCWM: %d\n", traceFileName, mode, NRA, NWA, NCRH, NCRM, NCWH, NCWM);
+    // CSV print
+    printf("%s,%d,%d,%d,%d,%d,%d,%d\n", traceFileName, cacheModeId, NRA, NWA, NCRH, NCRM, NCWH, NCWM);
+
+    // Pretty print
+    //printf("%s       \t\t%d\t\tNRA: %d    \t\t NWA: %d\t\t NCRH: %d\t\t RCRM: %d\t\t NCWH: %d\t\t NCWM: %d\n", traceFileName, cacheModeId, NRA, NWA, NCRH, NCRM, NCWH, NCWM);
 }
 
 int main (void) {
-    
-    FILE * testTraceFile;
-    testTraceFile = fopen("test.trc","r");
 
     FILE * bubbleTraceFile;
     bubbleTraceFile = fopen("bubble_sort_trace_019.trc","r");
@@ -110,7 +119,16 @@ int main (void) {
     FILE * crossCorrelationTraceFile;
     crossCorrelationTraceFile = fopen("cross_correlation_trace_019.trc","r");
 
-    run(testTraceFile, "test.trc", 1);
-    run(bubbleTraceFile, "bubble_sort_trace_019.trc", 1);
-    run(crossCorrelationTraceFile, "cross_correlation_trace_019.trc", 1);
+    for (int i = 1; i <= 8; i++) {
+        runCacheTest(bubbleTraceFile, "bubble_sort_trace_019.trc", i);
+        fseek(bubbleTraceFile, 0, SEEK_SET); // Reset file pointer to start of file
+    }
+
+    for (int i = 1; i <= 8; i++) {
+        runCacheTest(crossCorrelationTraceFile, "cross_correlation_trace_019.trc", i);
+        fseek(crossCorrelationTraceFile, 0, SEEK_SET); // Reset file pointer to start of file
+    }
+
+    fclose(bubbleTraceFile);
+    fclose(crossCorrelationTraceFile);
 }
